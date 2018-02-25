01d60e5c6c7f3d8edb2b587d9916cafbd94211e6
Archarious <archarious@gmail.com>
2018-02-25 13:28:58 +0300

    styling Neo.js

--- src/states/Game/sprites/persons/Neo.js
+++ src/states/Game/sprites/persons/Neo.js
@@ -5,42 +5,14 @@ const SPRITE_FILE = 'persons/neo.png'
 const FRAMES_FILE = 'persons/neo.json'
 
 export default class extends Animated {
-  static getAsset () {
-    return [ASSET_NAME, SPRITE_FILE, FRAMES_FILE]
-  }
+  static getAsset () { return [ASSET_NAME, SPRITE_FILE, FRAMES_FILE] }
 
   constructor ({game, x, y, type = 1}) {
-    const params = {
-      game,
-      x,
-      y,
-      asset: 'neo'
-    }
-    super(params)
+    super({ game, x, y, asset: ASSET_NAME })
 
     if (type === 2) this.frame = 49
-
-    this.addAnimation({
-      name: 'go',
-      prefix: `neo${type}_go_`,
-      length: 24,
-      speed: 24,
-      loop: true
-    })
-
-    this.addAnimation({
-      name: 'stop_hit',
-      prefix: `neo${type}_hit_`,
-      length: 24,
-      speed: 24
-    })
-
-    this.addAnimation({
-      name: 'hit',
-      prefix: `neo${type}_hit_`,
-      length: 24,
-      speed: 24,
-      offset: 13
-    })
+    this.addAnimation({ name: 'go', prefix: `neo${type}_go_`, length: 24, speed: 24, loop: true })
+    this.addAnimation({ name: 'stop_hit', prefix: `neo${type}_hit_`, length: 24, speed: 24 })
+    this.addAnimation({ name: 'hit', prefix: `neo${type}_hit_`, length: 24, speed: 24, offset: 13 })
   }
 }
