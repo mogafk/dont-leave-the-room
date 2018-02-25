22fbfaeec745152ac79e58ff3d957066a27f7582
Archarious <archarious@gmail.com>
2018-02-25 13:28:37 +0300

    append new events to scene

--- src/states/Game/game.js
+++ src/states/Game/game.js
@@ -5,21 +5,17 @@ import Chance from './sprites/Chance'
 import Layer from './sprites/Layer'
 import Hero from './sprites/persons/Hero'
 
-import Rape from './sprites/events/Rape'
-// import Doctor from './sprites/events/Doctor'
 import Brevik from './sprites/events/Brevik'
 import Nazi from './sprites/events/Nazi'
-
-import Event from './sprites/events/Event'
 
 import TrashCanEvent from './sprites/bgevents/baby-trash'
 import CrocEvent from './sprites/bgevents/croc'
 import PoliceEvent from './sprites/bgevents/police'
-
-import AllPersons from './sprites/AllPersons'
-const DEBUG_X_POS = 14
-const DEBUG_Y_POS = 14
-const DEBUG_COLOR = '#00ff00'
+import ExploseEvent from './sprites/bgevents/house-explose'
+import Montirovka from './sprites/bgevents/montirovka'
+import IcicleEvent from './sprites/bgevents/icicle'
+import RapeEvent from './sprites/bgevents/rape'
+// import AllPersons from './sprites/AllPersons'
 
 export default class extends Phaser.State {
   init () {
@@ -96,35 +92,52 @@ export default class extends Phaser.State {
     //   _sprite.anchor.setTo(0.5, 1)
     // //   create(_houseArray.pop())
     // }
-
- 
     // rape.x += 1700
+
+    const houseExplose = new ExploseEvent(this.game)
+    this.layer4.add(houseExplose)
+    houseExplose.x += 400
+    houseExplose.y = this.game.world.height - 400
+    houseExplose.play()
 
     const trashCanEvent = new TrashCanEvent(this.game, 0, 0)
     this.layer4.add(trashCanEvent)
 
     const police = this._police = new PoliceEvent(this.game)
     this.layer4.add(police)
-    police.x += 400
+    police.x += 600
     police.y = this.game.world.height - 25
     police.anchor.setTo(0.5, 1)
 
     const croc = this._croc = new CrocEvent(this.game)
     this.layer4.add(croc)
-    croc.x += 900
+    croc.x += 1500
     croc.y = this.game.world.height - 25
     croc.anchor.setTo(0.5, 1)
 
-    const rape = new Rape(this.game)
+    const rape = new RapeEvent(this.game)
     this.layer4.add(rape)
-    rape.x = 2200
+    rape.x = 2500
     rape.y = this.game.world.height * 1.25
+
+    this.icicle = new IcicleEvent(this.game, 0, 0)
+    this.layer4.add(this.icicle)
+    this.icicle.anchor.setTo(0.5, 1)
+    this.icicle.y += 100
 
     this.hero = new Hero({
       game: this.game,
       x: 70,
       y: this.game.world.height - 100
     })
+
+    this.layer5 = this.game.add.group()
+
+    this.montirovka = new Montirovka(this.game, 0, 0)
+    this.montirovka.scale.setTo(1.33)
+    this.montirovka.anchor.setTo(0.5, 0.5)
+    this.montirovka.y = this.game.world.height - 425
+    this.layer5.add(this.montirovka)
 
     const heroTarget = new Phaser.Point(this.game.world.centerX - 50, this.game.world.height - 100)
     // this.hero.anim['walk'].play()
@@ -156,6 +169,8 @@ export default class extends Phaser.State {
         this.hero.anim['stop'].play()
           .add(() => this.hero.anim['death'].play())
       })
+
+      this.hero.bringToTop()
     })
 
     scenarios.push(() => {
@@ -173,6 +188,8 @@ export default class extends Phaser.State {
         this.heroAlive = false
         this.hero.anim['death'].play()
       })
+
+      this.hero.bringToTop()
     })
 
     // let _houseArray = [
@@ -225,6 +242,7 @@ export default class extends Phaser.State {
       this.block = true
       this.isMove = false
       this.game.rnd.pick(scenarios)()
+      this.game.world.bringToTop(this.layer5)
       this.hero.anim['death'].onComplete.add(() => {
         this.buttonReplay = this.game.add.button(
           this.game.world.width - 150,
@@ -274,6 +292,7 @@ export default class extends Phaser.State {
       // this.layer3.move()
       // this.layer4.move()
       this.layer4.addAll('x', -2)
+      this.layer5.addAll('x', -3)
       this.zabor.tilePosition.x -= 5
       if (!this._police.activated && (this._police.x + 200) < this.game.world.centerX) {
         this._police.play()
@@ -281,6 +300,9 @@ export default class extends Phaser.State {
       // console.log(this._croc.x)
       if (!this._croc.activated && this._croc.x < this.game.world.centerX) {
         this._croc.play()
+      }
+      if (!this.icicle.activated && this.icicle.x < this.game.world.centerX + 100) {
+        this.icicle.play()
       }
     }
   }
